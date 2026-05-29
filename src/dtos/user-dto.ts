563dc5239b360b1989e10a56e1a716fc757905d3
Topalu Alin <alintopalu05@gmail.com>
2026-05-29 14:12:20 +0300

    username

--- src/dtos/user-dto.ts
+++ src/dtos/user-dto.ts
@@ -2,10 +2,12 @@ export class UserDto {
     email: string;
     id: number;
     isActivated: boolean;
+    nume: string;
 
     constructor(model: any) {
         this.email = model.email;
         this.id = model.idUser || model._id;
-        this.isActivated = model.isActivated
+        this.isActivated = model.isActivated;
+        this.nume = model.nume;
     }
 }
